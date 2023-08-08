5d4aaf97e083fa8582e6dcae5f68d7477644f476
Sandy <sandyhuang0408@outlook.com>
2023-08-08 15:41:51 -0400

    whack a mole

--- mole.js
+++ mole.js
@@ -7,6 +7,7 @@ let clickable = false;
 function popUpRandomMole() {
   if (molesLeft <= 0) {
     document.querySelector('.sb__game-over').classList.remove('sb__game-over--hidden');
+    document.querySelector('.sb__mole-counter').classList.add('sb__game-over--hidden');
     return;
   }
 
@@ -21,7 +22,7 @@ function popUpRandomMole() {
   clickable = true;
 
   // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED
-  // moleHead.classList.remove('wgs__mole-head--hidden', 'wgs__mole-head--whacked');
+  moleHead.classList.remove('wgs__mole-head--hidden', 'wgs__mole-head--whacked');
 
   molesLeft -= 1;
   document.querySelector('.sb__moles').innerHTML = molesLeft;
@@ -52,10 +53,21 @@ window.addEventListener('DOMContentLoaded', () => {
       hideMole(event.target);
 
       // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED
-      // event.target.classList.add('wgs__mole-head--hidden');
+      event.target.classList.add('wgs__mole-head--hidden');
 
       // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED FOR THE BONUS
-      // event.target.classList.add('wgs__mole-head--whacked');
+      event.target.classList.add('wgs__mole-head--whacked');
     });
   }
 });
+
+// window.addEventListener('DOMContentLoaded', () => {
+
+//   setInterval(async () => {
+//     const moleHeads = document.querySelectorAll('.wgs__mole-head');
+//     for (let moleHead of moleHeads) {
+//       moleHead.classList.toggle('wgs__mole-head--hidden');
+//     }
+//   }, 1000);
+
+// });
